95ee61ca9e5762c3d65d71f71ae6e62e0d68831c
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2022-12-03 21:33:37 -0500

    update: left right swipe on gamelist

--- Components/VideoGame.js
+++ Components/VideoGame.js
@@ -2,7 +2,14 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 import styles from "./VideoGame.module.css"
 
-const getGameData = async (gamelist, setTemp) =>{
+
+
+var games_liked = []
+var games_disliked = []
+var global_array=[]
+
+
+const getGameData = async (gamelist, setTemp, index, setIndex) =>{
 
     var Array = []
 
@@ -24,7 +31,9 @@ const getGameData = async (gamelist, setTemp) =>{
         }
     }
 
-    setTemp(Card(Array))
+    global_array=[...Array]
+
+    setTemp(Card(Array, index, setIndex))
 
     console.log("done.")
     // if(res.status == 200){
@@ -32,13 +41,14 @@ const getGameData = async (gamelist, setTemp) =>{
     // }    
 }
 
-const Card = (temp)=>{
+const Card = (temp, index, setIndex)=>{
     
     console.log("called")
+    console.log(index)
+    console.log(Array.isArray(temp))
     if (Array.isArray(temp)){
-        var element = temp[0]
+        var element = temp[index]
         return(<div>
-
             <div className={styles.mainHeading}>
                 Personalize your video gaming experience
             </div>
@@ -48,11 +58,29 @@ const Card = (temp)=>{
             </div>
 
             <div className={styles.title}>
-                {element.name}
+                <u>{element.name}</u>
             </div>
+            
+            <div className={styles.button_1}>
+            <div className={styles.button_plus} onClick={()=>{
+                games_disliked.push(element.name)
+                setIndex(index+1)
+            }}></div>
+            </div>
+                
+
             <div className={styles.imageHolder}>
-                <img src={element.url} className={styles.image}/>
+            <img src={element.url} className={styles.image}/>
             </div>
+                
+            <div className={styles.button_2}> 
+            <div className={styles.button_plus_2} onClick={()=>{
+                games_liked.push(element.name)
+                setIndex(index+1)
+                console.log(index)  
+            }}></div>
+            </div>
+            
             <div className={styles.summary}>
                 {element.summary}
             </div>
@@ -69,6 +97,7 @@ export default function VideoGameComponent({gamelist, fetch, setFetch}){
     console.log("called")
     console.log(fetch)
     const [temp, setTemp]= useState(<div> </div>)
+    const [index, setIndex]= useState(0)
 
     useEffect(()=>{
         if(gamelist.length!==0 && (!fetch)){
@@ -83,11 +112,28 @@ export default function VideoGameComponent({gamelist, fetch, setFetch}){
                 console.log("cookie recieved successfully")
             })
     
-            getGameData(gamelist,setTemp)
+            getGameData(gamelist,setTemp,index,setIndex)
             setFetch(true)
     }
     
     },[gamelist])
+
+    useEffect(()=>{
+        console.log("inisde the useEffect")
+        console.log(Array.isArray(gamelist))
+
+        if(fetch && Array.isArray(gamelist) && (index < global_array.length) ){
+            console.log("index is")
+            console.log(index)
+            setTemp(Card(global_array, index, setIndex))
+        }
+
+        if(index == global_array.length){
+            console.log(games_liked)
+            console.log(games_disliked)
+        }
+        
+    },[index])
 
     
     // const [temp, setTemp]= useState(<div> </div>)
